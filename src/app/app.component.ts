f38eec7969832da33335764f50ab9256e73b374b
Miljan <miljancode@gmail.com>
2023-10-21 11:40:23 +0200

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './core/layout/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<router-outlet/>',
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  template: `
+    <app-header />
+    <main>
+      <router-outlet />
+    </main>
+  `,
 })
 export class AppComponent {}
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  title: 'BuyAt.store',
+  navLinks: [
+    {
+      label: 'Shoes',
+      path: '/shoes',
+    },
+    {
+      label: 'Jeans',
+      path: '/jeans',
+    },
+  ],
+};
