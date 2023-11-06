9455f07b88f1cd6154db7bb5d245d65111bad3f9
Miljan <miljancode@gmail.com>
2023-11-06 12:21:58 +0100

    fix(api): token verify bug

--- packages/api/src/middlewares/auth.middleware.ts
+++ packages/api/src/middlewares/auth.middleware.ts
@@ -18,7 +18,12 @@ export const authentication = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies['__buyat_jwt'] as string | undefined;
     if (!token) return next();
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    let decoded: string | jwt.JwtPayload;
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch {
+      decoded = '';
+    }
 
     if (typeof decoded === 'string' || !decoded.exp) {
       return next();
